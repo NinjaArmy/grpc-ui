155d0bd5d1a8b3f1cca357aceae2e6bbf9f7e372
longdayscoldnights <jm@bitsandlikes.de>
2022-06-21 23:15:29 +0200

    update accordingly to new object

--- server.js
+++ server.js
@@ -16,28 +16,114 @@ const userInterfaceProto = grpc.loadPackageDefinition(packageDefinition);
 const server = new grpc.Server();
 let userInterfaces = [
   { 
-    id: "1",
-    applianceId: "1", 
-    layout: "standard", 
-    notifications: "Program started",
+    id: 1,
+    applianceId: 1, 
+    Layout: {
+      id: 1,
+      ApplianceType: {
+        MANUFACTURER: "Bosch",
+        MODEL: "X-3"
+      },
+      // Question here
+      applianceType: "",
+      views: 1
+    },
+    View: {
+      id: 1,
+      ViewType: {
+        STANDBY: 1,  // 0==false || 1==true
+        IDENTIFICATION: "home",
+        HOMESCREEN: "standard",
+        ACTIVE_PROGRAM: "coffee",
+        LIBRARY: "bosch home",
+        PROGRAM_SETTINGS: "standard",
+      },
+      type: "home",
+    },
+    Notification: {
+      id: 1,
+      NotificationType: {
+        DOOR_OPEN: 0, // 0==false || 1==true
+        PROGRAM_FINISHED: 1,  // 0==false || 1==true
+        ADD_PROGRAM: "coffee",
+        FUNCTION_NOT_AVAILABLE: "Don't know what this is for",
+        EMPTY: 0, //  0==false || 1==true
+      },
+      type: "Success", // Success / Danger / Warning?
+      topic: "coffee",
+      description: "Makes a standard coffee"
+
+    },
+    userInterfaceParameters: {
+      font: "sans-serif",
+      fontSizeMultiplier: 1,
+      contrast: 3,
+      elementSize: 12,
+      //  0==false || 1==true
+      fontBold: 0,
+      fontItalic: 0,
+      fontUnderline: 0,
+    },
     categories: "Coffee",
-    userInterfaceParameters: "sans-serif",
     timestamp: "19.06.2022 - 22:44 "
   },
   { 
-    id: "2",
-    applianceId: "2", 
-    layout: "custom", 
-    notifications: "Program started",
-    categories: "Tea",
-    userInterfaceParameters: "sans-serif",
-    timestamp: "19.06.2022 - 23:51 "
+    id: 2,
+    applianceId: 2, 
+    Layout: {
+      id: 1,
+      ApplianceType: {
+        MANUFACTURER: "Siemens",
+        MODEL: "S12"
+      },
+      // Question here
+      applianceType: "",
+      views: 1
+    },
+    View: {
+      id: 2,
+      ViewType: {
+        STANDBY: 0,  // 0==false || 1==true
+        IDENTIFICATION: "home",
+        HOMESCREEN: "standard",
+        ACTIVE_PROGRAM: "espresso",
+        LIBRARY: "siemens home",
+        PROGRAM_SETTINGS: "standard",
+      },
+      type: "home",
+    },
+    Notification: {
+      id: 2,
+      NotificationType: {
+        DOOR_OPEN: 0, // 0==false || 1==true
+        PROGRAM_FINISHED: 1,  // 0==false || 1==true
+        ADD_PROGRAM: "espresso",
+        FUNCTION_NOT_AVAILABLE: "Don't know what this is for",
+        EMPTY: 0, //  0==false || 1==true
+      },
+      type: "Success", // Success / Danger / Warning?
+      topic: "espresso",
+      description: "Makes a standard espresso"
+
+    },
+    userInterfaceParameters: {
+      font: "sans-serif",
+      fontSizeMultiplier: 1,
+      contrast: 3,
+      elementSize: 12,
+      //  0==false || 1==true
+      fontBold: 0,
+      fontItalic: 0,
+      fontUnderline: 0,
+    },
+    categories: "espresso",
+    timestamp: "21.06.2022 - 22:54"
   },
 ];
 
 server.addService(userInterfaceProto.UserInterfaceService.service, {
   getAllUserInterface: (_, callback) => {
-    callback(null, userInterface);
+    callback(null, userInterfaces);
   },
   getUserInterface: (_, callback) => {
     const userInterfaceId = _.request.id;
@@ -72,6 +158,7 @@ server.bindAsync(
   "127.0.0.1:50051",
   grpc.ServerCredentials.createInsecure(),
   (error, port) => {
+    console.log("Server at port:", port);
     console.log("Server running at http://127.0.0.1:50051");
     server.start();
   }
--- test.js
+++ test.js
@@ -1,20 +1,65 @@
 const client = require("./client");
 
-client.getAllUserInterface({}, (error, userInterface) => {
+client.getAllUserInterface({}, (error, userInterfaces) => {
   if (!error) throw error;
-  console.log(userInterface);
+  for(let i = 0; i < userInterfaces.length; i++){
+    console.log(userInterfaces[i]);
+  }
 });
 
 // add userInterface
 client.addUserInterface(
-  {
-    id: "3",
-    applianceId: "3", 
-    layout: "standard", 
-    notifications: "Vorgang gestartet",
-    categories: "Kaffee",
-    userInterfaceParameters: "sans-serif",
-    timestamp: "20.06.2022 - 17:34 "
+  { 
+    id: 3,
+    applianceId: 3, 
+    Layout: {
+      id: 1,
+      ApplianceType: {
+        MANUFACTURER: "LG",
+        MODEL: "LG-Coffee"
+      },
+      // Question here
+      applianceType: "",
+      views: 1
+    },
+    View: {
+      id: 1,
+      ViewType: {
+        STANDBY: 1,  // 0==false || 1==true
+        IDENTIFICATION: "home",
+        HOMESCREEN: "standard",
+        ACTIVE_PROGRAM: "coffee",
+        LIBRARY: "LG home",
+        PROGRAM_SETTINGS: "standard",
+      },
+      type: "home",
+    },
+    Notification: {
+      id: 1,
+      NotificationType: {
+        DOOR_OPEN: 0, // 0==false || 1==true
+        PROGRAM_FINISHED: 1,  // 0==false || 1==true
+        ADD_PROGRAM: "coffee",
+        FUNCTION_NOT_AVAILABLE: "Don't know what this is for",
+        EMPTY: 0, //  0==false || 1==true
+      },
+      type: "Success", // Success / Danger / Warning?
+      topic: "coffee",
+      description: "Makes a standard coffee"
+
+    },
+    userInterfaceParameters: {
+      font: "sans-serif",
+      fontSizeMultiplier: 1,
+      contrast: 3,
+      elementSize: 12,
+      //  0==false || 1==true
+      fontBold: 0,
+      fontItalic: 0,
+      fontUnderline: 0,
+    },
+    categories: "Coffee",
+    timestamp: "19.06.2022 - 22:44 "
   },
   (error, userInterface) => {
     if (error) throw error;
@@ -22,8 +67,10 @@ client.addUserInterface(
   }
 );
 
-// edit a userInterface
-client.editUserInterface(
+/* edit a userInterface
+   Needs to be changed to fit the new Object
+*/
+/* client.editUserInterface(
   {
     id: "2 edited",
     applianceId: "2 edited", 
@@ -37,7 +84,7 @@ client.editUserInterface(
     if (error) throw error;
     console.log("Successfully edited a userInterface.");
   }
-);
+); */
 
 // delete a userInterface
 client.deleteUserInterface(
