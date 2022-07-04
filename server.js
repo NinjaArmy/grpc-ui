9effd9f133c0a88643141da77ed64869957e8a2a
longdayscoldnights <jm@bitsandlikes.de>
2022-07-05 01:59:56 +0200

    adding components and wizard

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ let userInterfaces = [
       font_underline: 0,
     },
     categories: "Coffee",
-    timestamp: "19.06.2022 - 22:44 "
+    timestamp: "19.06.2022 - 22:44"
   },
   { 
     id: 2,
@@ -116,12 +116,73 @@ let userInterfaces = [
       font_italic: 0,
       font_underline: 0,
     },
+
     categories: "espresso",
     timestamp: "21.06.2022 - 22:54"
   },
 ];
 
+let components = [
+  {
+    parameter_id: 1,
+    default_value: 1,
+    x: 1,
+    y: 1,
+    font_size: 12,
+    value_maps: {
+      "x": 1,
+      "y": 1
+    },
+    button_title: "start",
+    icon: "clock",
+    navigate_to: "Progress Page",
+    executable: true,
+    program_id: 123,
+    button: true
+  },
+  {
+    parameter_id: 2,
+    default_value: 2,
+    x: 2,
+    y: 2,
+    font_size: 12,
+    value_maps: {
+      "x": 2,
+      "y": 2
+    },
+    progress_bar: true
+  }
+];
+
+let wizards = [
+  {
+    title: "Standard",
+    Page: {
+      description: "Standard",
+      MenuInterface: {
+        options: ["Next", "Back"]
+      }
+    },
+    x: 1,
+    y: 1,
+    font_size: 12
+  },
+  {
+    title: "Progress",
+    Page: {
+      description: "Progressbar",
+      MenuInterface: {
+        options: "cancel"
+      }
+    },
+    x: 1,
+    y: 1,
+    font_size: 12
+  }
+];
+
 server.addService(userInterfaceProto.UserInterfaceService.service, {
+  /* UserInterface */
   getAllUserInterface: (_, callback) => {
     callback(null, userInterfaces);
   },
@@ -152,7 +213,95 @@ server.addService(userInterfaceProto.UserInterfaceService.service, {
     userInterfaces.push(_userInterface);
     callback(null, _userInterface);
   },
+
+  /* Components */
+  getAllComponents: (_, callback) => {
+    callback(null, components);
+  },
+  getComponent: (_, callback) => {
+    const componentId = _.request.id;
+    const componentItem = components.find(({ id }) => componentId == id);
+    callback(null, componentItem);
+  },
+  deleteComponent: (_, callback) => {
+    const componentId = _.request.id;
+    components = components.filter(({ id }) => id !== componentId);
+    callback(null, {});
+  },
+  editComponent: (_, callback) => {
+    const componentId = _.request.id;
+    const componentItem = components.find(({ id }) => componentId == id);
+    componentItem.parameter_id = _.request.parameter_id;
+    componentItem.default_value = _.request.default_value;
+    componentItem.x = _.request.x;
+    componentItem.y = _.request.y;
+    componentItem.font_size = _.request.font_size;
+    componentItem.value_maps = _.value_maps.value_maps;
+
+    if(componentItem.button) {
+      componentItem.button_title = _.request.button_title;
+      componentItem.icon = _.request.icon;
+      componentItem.navigate_to = _.request.navigate_to;
+      componentItem.executable = _.request.executable;
+    }
+
+    if(componentItem.drop_down) {
+      componentItem.values = _.request.values;
+    }
+
+    if(componentItem.slider) {
+      componentItem.id = _.request.id;
+      componentItem.minimum = _.request.minimum;
+      componentItem.maximum = _.request.maximum;
+      componentItem.step = _.request.step;
+      componentItem.vertical = _.request.vertical;
+      componentItem.legend = _.request.legend;
+      componentItem.counter_slider = _.request.counter_slider;
+      componentItem.width = _.request.width;
+      componentItem.unit = _.request.unit;
+    }
+    componentItem.time_selector = _.request.time_selector;
+    callback(null, componentItem);
+  },
+  addComponent: (call, callback) => {
+    let _component = { id: Date.now(), ...call.request };
+    userInterfaces.push(_component);
+    callback(null, _component);
+  },
+
+  /* Wizards */
+  getAllWizards: (_, callback) => {
+    callback(null, wizards);
+  },
+  getWizard: (_, callback) => {
+    const wizardId = _.request.id;
+    const wizardItem = wizards.find(({ id }) => wizardId == id);
+    callback(null, wizardItem);
+  },
+  deleteWizard: (_, callback) => {
+    const wizardId = _.request.id;
+    wizards = wizards.filter(({ id }) => id !== wizardId);
+    callback(null, {});
+  },
+  editWizard: (_, callback) => {
+    const wizardId = _.request.id;
+    const wizardItem = wizards.find(({ id }) => wizardId == id);
+    wizardItem.title = _.request.title;
+    wizardItem.Page = _.request.Page;
+    wizardItem.pages = _.request.pages;
+    wizardItem.x = _.request.x;
+    wizardItem.y = _.request.y;
+    wizardItem.font_size = _.request.font_size;
+    callback(null, wizardItem);
+  },
+  addWizard: (call, callback) => {
+    let _wizard = { title: Date.now(), ...call.request };
+    wizards.push(_wizard);
+    callback(null, _wizard);
+  },
 });
+
+
 
 server.bindAsync(
   "127.0.0.1:50051",
